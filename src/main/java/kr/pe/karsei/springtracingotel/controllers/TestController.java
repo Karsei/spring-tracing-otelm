d54a0da222dbcdf2dbb4a3bab28483493142ec9b
Kai <leejy@gabia.com>
2022-06-08 15:26:11 +0900

    feat: zipkin 으로 이용할 경우 추가

--- src/main/java/kr/pe/karsei/springtracingotel/controllers/TestController.java
+++ src/main/java/kr/pe/karsei/springtracingotel/controllers/TestController.java
@@ -1,12 +1,17 @@
 package kr.pe.karsei.springtracingotel.controllers;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
+@Slf4j
 @RestController
 @RequestMapping(value = "api1")
 @RequiredArgsConstructor
@@ -14,13 +19,16 @@ public class TestController {
     private final RestTemplate restTemplate;
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(@RequestHeader Map<String, String> headers) {
+        headers.forEach((k, v) -> log.info("Hello - Request Header '{}' = {}", k, v));
         return "hello, world!";
     }
 
     @GetMapping("call")
-    public String call() {
-        String url = "http://localhost:7001/api1/hello";
+    public String call(@RequestHeader Map<String, String> headers) {
+        headers.forEach((k, v) -> log.info("Call - Request Header '{}' = {}", k, v));
+
+        String url = "http://10.43.252.208/api1/hello";
 
         StringBuilder sb = new StringBuilder();
         try {
